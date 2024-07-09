82442d5232ac69b80f5204cd85095f86a73bb6b3
Yash Raj Sharma <154724719+Yash1210codeing@users.noreply.github.com>
2024-07-09 21:25:34 +0530

    Add files via upload

---
+++ OOPS_Chapter_2/Stack_Java_Print.java
@@ -0,0 +1,14 @@
+package OOPS_Chapter_2;
+import java.util.Stack;
+public class Stack_Java_Print {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack();
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		st.push(40);
+		st.push(50);
+		System.out.println(st);
+	}
+}
